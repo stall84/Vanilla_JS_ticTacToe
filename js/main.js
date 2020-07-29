721ee531416829c93251e8d5ff1443932d1e5084
Michael Stallings <stall84@gmail.com>
2020-07-28 20:24:39 -0400

    adding basic setup, html and css completed. working on factory functions in js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+
+
+// Create Player function factory 
+
+const playerFactory = (name, mark) => {
+    const playTurn = (board, cell) => {
+        const idx = board.cells.findIndex(position => position === cell);
+    }
+}
